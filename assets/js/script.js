1526383c0bd8cf13ea2aaedee365be6d402069ff
TanyaElizabeth13 <tanya_allen400@hotmail.co.uk>
2022-11-08 14:31:41 +0000

    generate card data

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//Generate card images
+const imgData = () => [
+    { imgSrc: "./assets/images/Audrey-aubergine.jpg", name: "aubergine"},
+    { imgSrc: "./assets/images/audrey-text.jpg", name: "aubergine"},
+    { imgSrc: "./assets/images/Brenda-broccoli.jpg", name: "broccoli"},
+    { imgSrc: "./assets/images/brenda-text.jpg", name: "broccoli"},
+    { imgSrc: "./assets/images/Caroline-carrot.jpg", name: "carrot"},
+    { imgSrc: "./assets/images/caroline-text.jpg", name: "carrot"},
+    { imgSrc: "./assets/images/Lisa-lettuce.jpg", name: "lettuce"},
+    { imgSrc: "./assets/images/lisa-text.jpg", name: "lettuce"},
+    { imgSrc: "./assets/images/Oliver-onion.jpg", name: "onion"},
+    { imgSrc: "./assets/images/oliver-text.jpg", name: "onion"},
+    { imgSrc: "./assets/images/Peter-potato.jpg", name: "potato"},
+    { imgSrc: "./assets/images/peter-text.jpg", name: "potato"},
+]
+
+
